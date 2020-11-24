195f713333ebc14548c01e2ef3c379565a12b8b6
markus <markus.norell@gmail.com>
2020-11-24 21:52:43 +0100

    ,

---
+++ dot.config/bspwm/refresh_spaces.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+bspc monitor -d \
+"1. $(cat ~/.config/bspwm/spaces/1)" \
+"2. $(cat ~/.config/bspwm/spaces/2)" \
+"3. $(cat ~/.config/bspwm/spaces/3)" \
+"4. $(cat ~/.config/bspwm/spaces/4)" \
+"5. $(cat ~/.config/bspwm/spaces/5)" \
+"6. $(cat ~/.config/bspwm/spaces/6)" \
+"7. $(cat ~/.config/bspwm/spaces/7)" \
+"8. $(cat ~/.config/bspwm/spaces/8)" \
+"9. $(cat ~/.config/bspwm/spaces/9)" \
+"10. $(cat ~/.config/bspwm/spaces/10)" \
+
