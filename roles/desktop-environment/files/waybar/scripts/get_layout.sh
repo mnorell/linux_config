6404b86d97597241d7b036c7076008bfc7107562
markus.norell@gmail.com <markus.norell@gmail.com>
2025-12-04 23:24:56 +0100

    per keyboard layouts

--- roles/desktop-environment/files/waybar/scripts/get_layout.sh
+++ roles/desktop-environment/files/waybar/scripts/get_layout.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-swaymsg -t get_inputs | grep '"xkb_active_layout_name": "alt for swedish' >/dev/null
-if [ $? -eq 0 ]; then
-    echo gb
-    exit 0
-fi
+cherry_id="1130:17:HID_046a:0011"
+dell_id="1:1:AT_Translated_Set_2_keyboard"
 
-swaymsg -t get_inputs | grep '"xkb_active_layout_name": "Swedish' >/dev/null
-if [ $? -eq 0 ]; then
-    echo se
-    exit 0
-fi
+us_gb_layout_name="alt for swedish"
+se_layout_name="Swedish"
+
+
+main () {
+    cherry_layout=$(get_layout $cherry_id)
+    dell_layout=$(swaymsg -t get_inputs | jq -r --arg id "$dell_id" '.[] | select(.identifier == $id) | .xkb_active_layout_name')
+
+    if [[ "$dell_layout" == "$us_gb_layout_name"* ]]; then
+        if [[ "$cherry_layout" == "$us_gb_layout_name"* ]]; then
+            echo gb,us
+        elif [[ "$cherry_layout" == "$se_layout_name"* ]]; then
+            echo gb,se
+        else
+            echo gb
+        fi
+    else
+        if [[ "$cherry_layout" == "$us_gb_layout_name"* ]]; then
+            echo se,us
+        elif [[ "$cherry_layout" == "$se_layout_name"* ]]; then
+            echo se,se
+        else
+            echo se
+        fi
+    fi
+}
+
+get_layout() {
+    swaymsg -t get_inputs | jq -r --arg id "$1" '.[] | select(.identifier == $id) | .xkb_active_layout_name'
+}
+
+main
