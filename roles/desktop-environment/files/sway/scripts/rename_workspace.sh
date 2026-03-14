ff6a0bfaf19f8f0c36d488609da2f7e7ad986ecc
markus.norell@gmail.com <markus.norell@gmail.com>
2026-03-14 01:46:13 +0100

    fuzzel for renaming workspace

--- roles/desktop-environment/files/sway/scripts/rename_workspace.sh
+++ roles/desktop-environment/files/sway/scripts/rename_workspace.sh
@@ -5,13 +5,13 @@ name=$(swaymsg -t get_workspaces -r | jq '.[] | select(.focused==true) | .name')
 num=$(     echo $name | sed 's/\"\(.*\)\:\(.*\)\"/\1/')
 old_name=$(echo $name | sed 's/\"\(.*\)\:\(.*\)\"/\2/')
 
-new_name=$(rofi -dmenu \
-    -p "Rename workspace" \
-    -font "JetBrains Mono NF 16" \
-    -no-fixed-num-lines \
-    -width 20 \
-    -filter "$old_name")
+# new_name=$(rofi -dmenu \
+#     -p "Rename workspace" \
+#     -font "JetBrains Mono NF 16" \
+#     -no-fixed-num-lines \
+#     -width 20 \
+#     -filter "$old_name")
 
-# new_name=$(fuzzel --dmenu --lines 0 --prompt "Rename workspace: ")
+new_name=$(fuzzel --dmenu --lines 0 --prompt "Rename workspace:" --search "$old_name")
 
 swaymsg rename workspace to $num:$new_name
