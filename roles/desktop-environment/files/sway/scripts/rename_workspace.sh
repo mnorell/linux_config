af4bd6f68785ae037587a8f243fd26d40806bcfa
markus.norell@gmail.com <markus.norell@gmail.com>
2026-03-22 04:14:24 +0100

    blank workspace names

--- roles/desktop-environment/files/sway/scripts/rename_workspace.sh
+++ roles/desktop-environment/files/sway/scripts/rename_workspace.sh
@@ -7,4 +7,9 @@ old_name=$(echo $name | sed 's/\"\(.*\)\:\(.*\)\"/\2/')
 
 new_name=$(fuzzel --dmenu --width 60 --prompt-only "Rename workspace:" --search "$old_name")
 
+# Make the workspace switching work after rename
+id='$ws'$num
+cmd="set \"$id\" $num:$new_name"
+swaymsg $cmd
+
 swaymsg rename workspace to $num:$new_name
